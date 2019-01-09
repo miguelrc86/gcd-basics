52fe8306ada4f0d47074a90ba44c32b334301edb
Miguel Rojas <miguel.rojas@endava.com>
2019-01-09 16:33:10 -0500

    Replacing wait() strategy by notify()

--- GooglyPuff/PhotoManager.swift
+++ GooglyPuff/PhotoManager.swift
@@ -71,28 +71,25 @@ final class PhotoManager {
     }
     
     func downloadPhotos(withCompletion completion: BatchPhotoDownloadingCompletionClosure?) {
-        DispatchQueue.global(qos: .userInitiated).async {
-            var storedError: NSError?
-            let downloadGroup = DispatchGroup()
-            
-            for address in [PhotoURLString.overlyAttachedGirlfriend,
-                            PhotoURLString.successKid,
-                            PhotoURLString.lotsOfFaces] {
-                                let url = URL(string: address)
-                                downloadGroup.enter()
-                                let photo = DownloadPhoto(url: url!) { _, error in
-                                    if error != nil {
-                                        storedError = error
-                                    }
-                                    downloadGroup.leave()
+        var storedError: NSError?
+        let downloadGroup = DispatchGroup()
+        
+        for address in [PhotoURLString.overlyAttachedGirlfriend,
+                        PhotoURLString.successKid,
+                        PhotoURLString.lotsOfFaces] {
+                            let url = URL(string: address)
+                            downloadGroup.enter()
+                            let photo = DownloadPhoto(url: url!) { _, error in
+                                if error != nil {
+                                    storedError = error
                                 }
-                                PhotoManager.shared.addPhoto(photo)
-            }
-            
-            downloadGroup.wait()
-            DispatchQueue.main.async {
-                completion?(storedError)
-            }
+                                downloadGroup.leave()
+                            }
+                            PhotoManager.shared.addPhoto(photo)
+        }
+        
+        downloadGroup.notify(queue: DispatchQueue.main) {
+            completion?(storedError)
         }
     }
     
